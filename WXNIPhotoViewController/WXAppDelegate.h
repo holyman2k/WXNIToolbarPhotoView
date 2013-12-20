ca1c379cd67bfd048fbb7690df5c15ec6b5c0877
Charlie Wu <charlie.y.wu@gmail.com>
2013-12-20 12:40:44 +1100

    Initial Commit

---
+++ WXNIPhotoViewController/WXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WXAppDelegate.h
+//  WXNIPhotoViewController
+//
+//  Created by Charlie Wu on 20/12/2013.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WXNIPhotoViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WXNIPhotoViewController
+//
+//  Created by Charlie Wu on 20/12/2013.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WXAppDelegate class]));
+    }
+}
